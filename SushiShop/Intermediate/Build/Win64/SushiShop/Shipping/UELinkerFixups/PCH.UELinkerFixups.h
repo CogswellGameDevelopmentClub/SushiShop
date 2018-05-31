14dae9ff72f8aab4e987e0915ab6f8598b65c4e5
pvalenzuela1 <35516648+pvalenzuela1@users.noreply.github.com>
2018-05-30 18:30:05 -0700

    Updated project to 4.19

--- SushiShop/Intermediate/Build/Win64/SushiShop/Shipping/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\GameDevClub\Documents\GitHub\SushiShop\SushiShop\Intermediate\Build\Win64\UE4\Shipping\UELinkerFixupsName.h
-#include "C:\Users\GameDevClub\Documents\GitHub\SushiShop\SushiShop\Intermediate\Build\Win64\UE4\Shipping\UELinkerFixupsName.h"
